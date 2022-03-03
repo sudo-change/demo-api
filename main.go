d93689c7b99e4c264da394af89b3a5ea14beb424
sudo change <dragoondranzerkai@gmail.com>
2022-03-03 22:15:15 +0530

    first

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 
 // handler
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippetbox"))
 }
 
@@ -18,7 +22,8 @@ func main() {
 	mux.HandleFunc("/", home)
 
 	log.Println("starting server on: 4000")
-	// http.ListenAndServe() to start
+	// http.ListenAndServe() to  a new web server
+	// if it returns an error, we use log.Fatal() function to log the error message and exit
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 
